108b32d077694f74ce7760b1e9bf4b885eca509d
darrennnr <dpnarendra67@gmail.com>
2025-09-28 15:18:17 +0700

    sqlite migration

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:vosk_stt/stt/main/stt.dart';
 
 Future<void> main() async {
   // Pastikan Flutter binding sudah diinisialisasi
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inisialisasi koneksi Supabase
-  await Supabase.initialize(
-    url: 'https://xqguweftjklmzmrwtqet.supabase.co', // Ganti dengan URL Supabase kamu
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxZ3V3ZWZ0amtsbXptcnd0cWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNzYxNjgsImV4cCI6MjA2OTk1MjE2OH0.JckKnoCcns114jhSUfycEmrnUWiUTySR2P0xfzBhkDU', // Ganti dengan anon key dari Supabase Project Settings
-  );
+  // REMOVED: Supabase initialization - tidak diperlukan lagi
+  // SQLite databases akan diinisialisasi langsung di QuranSQLiteService
 
   runApp(const MainApp());
 }
@@ -22,7 +18,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: VoskSTTCorrectionPage(suratId: 36, suratName: 'يٰسۤ',),
+      home: VoskSTTCorrectionPage(suratId: 36),
     );
   }
 }
